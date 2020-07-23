f813e6b0742e5b2389f243224eda001c4924aaa5
DoggoMan <robspinoglio@gmail.com>
2020-07-23 20:14:53 +1000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react'
+
+const App: FC = () => {
+  return (
+    <div>
+      <h1>Hello!</h1>
+
+      <ul>
+        <li>Create Game</li>
+        <li>List Games</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
